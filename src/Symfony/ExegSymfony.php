7c9b5eb7c23f92a14408b63d511ba862036b2f35
Gerrit Drost <gdrost@vcn.nl>
2019-08-20 10:41:29 +0200

    Added possibility to set default timeout to ExegSymfony (#3)
    Added possibility to set default timeout to ExegSymfony

--- src/Symfony/ExegSymfony.php
+++ src/Symfony/ExegSymfony.php
@@ -8,20 +8,50 @@ use Vcn\Exeg\Command;
 
 class ExegSymfony
 {
+    /**
+     * @var null|callable
+     */
     private $defaultCallback = null;
 
-    public function __construct(?callable $defaultCallback = null)
+    /**
+     * @var null|int
+     */
+    private $defaultTimeout = null;
+
+    /**
+     * @param null|callable $defaultCallback  Process callback. {@See Process::run()} for details.
+     * @param null|int      $defaultTimeout   Timeout in seconds. Null to use Symfony's default, 0 to disable timeouts.
+     */
+    public function __construct(?callable $defaultCallback = null, ?int $defaultTimeout = null)
     {
         if (!class_exists(Process::class)) {
             throw new RuntimeException(sprintf("Please install symfony/process to make use of %s", self::class));
         }
 
         $this->defaultCallback = $defaultCallback;
+        $this->defaultTimeout  = $defaultTimeout;
     }
 
+    /**
+     * @param null|callable $callback Process callback. {@See Process::run()} for details.
+     *
+     * @return ExegSymfony
+     */
     public function setDefaultCallback(?callable $callback): self
     {
         $this->defaultCallback = $callback;
+
+        return $this;
+    }
+
+    /**
+     * @param null|int $defaultTimeout Timeout in seconds. Null to use Symfony's default, 0 to disable timeouts.
+     *
+     * @return ExegSymfony
+     */
+    public function setDefaultTimeout(?int $defaultTimeout): self
+    {
+        $this->defaultTimeout = $defaultTimeout;
 
         return $this;
     }
@@ -44,6 +74,10 @@ class ExegSymfony
 
         if ($env !== null) {
             $process->setEnv($env);
+        }
+
+        if ($this->defaultTimeout !== null) {
+            $process->setTimeout($this->defaultTimeout);
         }
 
         return $process;
